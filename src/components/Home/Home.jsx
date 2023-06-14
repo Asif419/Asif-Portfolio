01e63f0a8314d6d969c5558ed4fa7174ebd46feb
Asif419 <asshah419@gmail.com>
2023-06-14 10:36:29 +0600

    router set up

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h2>home okay</h2>
+    </div>
+  );
+};
+
+export default Home;<h2>home okay</h2>
---
+++ src/layout/Main.jsx
@@ -0,0 +1,9 @@
+import Home from "../components/Home/Home";
+
+const Main = () => {
+  return (
+    <Home></Home>
+  );
+};
+
+export default Main;
